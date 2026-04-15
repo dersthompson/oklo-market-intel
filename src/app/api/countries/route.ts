98e2f97de612f35b4fd0531fddf0123fb5f2c25e
dersthompson <ander.thompson11@gmail.com>
2026-04-14 18:36:57 -0700

    Remove old countries list route

--- src/app/api/countries/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import type { Country } from "@/lib/types";
-import countriesData from "../../../../data/countries.json";
-
-export const dynamic = "force-static";
-
-export function GET() {
-  return NextResponse.json(countriesData as Country[]);
-}
